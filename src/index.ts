30aa5ec1f2658c1dd9d900ad25df5be1929fb134
spaghetti <alejandrolpz0619@gmail.com>
2021-07-13 16:17:26 -0400

    minor changes

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,13 @@ import { BASE_URL } from './lib/keys.js';
 const app = express();
 
 //setting:
-app.set('port', 3000 || process.env.PORT);
+app.set('port', process.env.PORT || 5000);
 
 //Routes:
 app.use(BASE_URL, router)
 //Middleware:
 app.use(express.urlencoded({ extended: false }));
-
+app.use(express.json())
 //start:
 app.listen(app.get('port'), () => {
   console.log(`server on port ${app.get('port')}`)
