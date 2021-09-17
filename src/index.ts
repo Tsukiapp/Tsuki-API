8e0d471a31c4d035342eb09f1cc938898e5f7e45
alejandro Lopez <alejandrolpz0619@gmail.com>
2021-09-17 15:52:32 -0400

    🚀Changes: src/index
    files created / modified
    
    Datails:
    created an own logger.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 // import:
-import express from 'express';
+import express, { Request, Response, NextFunction} from 'express';
 import router from './routes/routes.js';
 import { BASE_URL } from './lib/keys.js';
 import cors from 'cors';
+import chalk from 'chalk';
+
+
 //initialize:
 const app = express();
 
@@ -10,6 +13,17 @@ const app = express();
 app.set('port', process.env.PORT || 5000);
 //cors:
 app.use(cors());
+// Logger:
+app.use((req: Request, res: Response, next: NextFunction) => {
+  const { url, method } = req;
+  const log =
+    `
+    ${chalk.blue('mehtod: ')}${chalk.cyan(method)}
+    ${chalk.blue('url: ')}${chalk.cyan(url)}
+    `
+  console.log(log);
+  next();
+});
 //Routes:
 app.use(BASE_URL, router);
 //Middleware:
@@ -17,5 +31,5 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 //start:
 app.listen(app.get('port'), () => {
-  console.log(`server on port ${app.get('port')}`);
+  console.log(`server on port ${chalk.green(app.get('port'))}`);
 });
